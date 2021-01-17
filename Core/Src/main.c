b9bf8612596e7acee02c3fc398bab21bd003cb1d
ikonsty <i.konsty@gmail.com>
2021-01-17 13:27:11 +0200

    Get all, but bad steps

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -44,9 +44,8 @@
 /* USER CODE BEGIN PD */
 #define PRINT(X)			CDC_Transmit_FS(X, strlen(X))
 //#define ARM_MATH_CM4
-#define LENGTH				128
 
-const uint16_t FFT_SIZE = 512;
+const uint16_t FFT_SIZE = 1024;
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -60,9 +59,7 @@ const uint16_t FFT_SIZE = 512;
 CRC_HandleTypeDef hcrc;
 
 I2S_HandleTypeDef hi2s2;
-I2S_HandleTypeDef hi2s3;
 DMA_HandleTypeDef hdma_spi2_rx;
-DMA_HandleTypeDef hdma_spi3_tx;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -74,94 +71,92 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-uint16_t txBuf[128];
-uint16_t pdmRxBuf[1024];
-uint16_t MidBuffer[32];
+uint16_t pdmRxBuf[128];
+uint16_t MidBuffer[16];
 uint8_t rxstate = 0;
 
+uint16_t pcm_buffer[2048];
+float32_t pcm_fl_buffer[2048];
+int pcm_index = 0;
+char re_im[100];
+float real_freq = 0.0;
 
-uint16_t fifobuf[256];
-uint8_t fifo_w_ptr = 0;
-uint8_t fifo_r_ptr = 0;
-uint8_t fifo_read_enabled = 0;
-float fft_out_buf[2048];
+
+float32_t fft_out_buf[2048];
 int Fs = 48076;
 int N = 1024;
+int pcm_full = 0;
 
-char str_res[5];
+char str_res[100];
 
 arm_rfft_fast_instance_f32 fft_handler;
 
 int i;
-float String_Freq[] = {82.41, 110, 146.83, 196, 246.94, 329.63}; // From S1(thickest) to S6(thinnest)
+//float String_Freq[] = {82.41, 110, 146.83, 196, 246.94, 329.63}; // From S1(thickest) to S6(thinnest)
+float String_Freq[] = {34.0, 93.0, 140.0, 187.0, 234.0, 328.0};
 
-void FifoWrite(uint16_t data) {
-	fifobuf[fifo_w_ptr] = data;
-	fifo_w_ptr++;
-}
-
-uint16_t FifoRead() {
-	uint16_t val = fifobuf[fifo_r_ptr];
-	fifo_r_ptr++;
-	return val;
-}
 
 void ReadSample() {
 	  if(rxstate == 1) {
-		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, 1);
-		  HAL_Delay(50);
-		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, 0);
-		  HAL_Delay(50);
 		  PDM_Filter(&pdmRxBuf[0], &MidBuffer[0], &PDM1_filter_handler); //MidBuffer is the pcm data
 
-//		  for(int i = 0; i < 16; i++) {FifoWrite(MidBuffer[i]);}
-//		  if(fifo_w_ptr - fifo_r_ptr > 128) fifo_read_enabled = 1;
+		  for(int i = 0; i < 16; i++) {
+			  pcm_buffer[pcm_index] = MidBuffer[i];
+			  pcm_fl_buffer[pcm_index] = (float32_t)pcm_buffer[pcm_index];
+			  pcm_index++;
+		  }
+		  if(pcm_index == 2048) {
+			  pcm_full = 1;
+			  pcm_index = 0;
+		  }
 		  rxstate = 0;
 	  }
 
 	  if(rxstate == 2) {
-		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, 1);
-		  HAL_Delay(50);
-		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, 0);
-		  HAL_Delay(50);
 		  PDM_Filter(&pdmRxBuf[64], &MidBuffer[0], &PDM1_filter_handler);
-//		  for(int i = 0; i < 16; i++) {FifoWrite(MidBuffer[i]); }
+		  for(int i = 0; i < 16; i++) {
+			  pcm_buffer[pcm_index] = MidBuffer[i];
+			  pcm_fl_buffer[pcm_index] = (float32_t)pcm_buffer[pcm_index];
+			  pcm_index++;
+		  }
+		  if(pcm_index == 2048) {
+			  pcm_full = 1;
+			  pcm_index = 0;
+		  }
 		  rxstate = 0;
 	  }
 }
 
-float complexABS(float real, float compl) {
-	return sqrtf(real*real+compl*compl);
-}
-
-int largest_index(float arr[], int len) {
-	int max = 0;
-	for (int i = 1; i < len / 2 + 1; i++) {
-		if (arr[i] > arr[max]) {
-			max = i;
-		}
-	}
-	return max;
-}
-
-float DoFFT_find_freq(float* fft_in_buf) {
+float DoFFT_find_freq() {
 	//Do FFT
-	arm_rfft_fast_f32(&fft_handler, fft_in_buf, fft_out_buf, 0);
+	arm_rfft_fast_f32(&fft_handler, &pcm_fl_buffer, &fft_out_buf, 0);
 
 	float freqs[1024];
 	int freqpoint = 0;
-	int offset = 150; //variable noisefloor offset
+	float max_magnitude = 0;
+	int max_i = 0;
+
 
 	//calculate abs values and linear-to-dB
-	for (int i = 0; i < 2048; i = i + 2) {
-		freqs[freqpoint] = (float)(20*log10f(complexABS(fft_out_buf[i], fft_out_buf[i+1])))-offset;
-		if (freqs[freqpoint] < 0) freqs[freqpoint] = 0;
-		freqpoint++;
-	}
-	int f_ln = sizeof(freqs)/sizeof(freqs[0]);
-	float l_index = largest_index(freqs, f_ln);
+	for (int i = 1; i < 1024; i++) {
 
-	float frequency = l_index * Fs / N;
+		float32_t re = fft_out_buf[2*i];
+		float32_t im = fft_out_buf[2*i+1];
+		float magn = (float)sqrt(re*re+im*im);
+		if (magn >= max_magnitude) {
+			max_magnitude = magn;
+			max_i = i;
+		}
+
+	}
+
+	float frequency = max_i * Fs / N;
+	if(frequency > 500.0) {
+		frequency = 0.0;
+	}
+//	char str_res[100];
+//	sprintf(str_res, "index = %i; max_magn = %f,  frequency = %f\n", max_i, max_magnitude, frequency);
+//	PRINT(str_res);
 	return frequency;
 }
 
@@ -182,46 +177,87 @@ char* concat(const char *s1, const char *s2)
 
 char* detectString(float freq) {
 	int String = 0;
-	if (freq <= String_Freq[0]) String = 1;
-	if (freq >= String_Freq[5]) String = 6;
+	if (freq > 0) real_freq = freq;
+//	if (real_freq <= String_Freq[0]) String = 6;
+//	if (real_freq >= String_Freq[5]) String = 1;
 
-	int m = 0, j = 6, mid = 0;
-	while (m < j) {
-		mid = (m+j)/2;
-		if (freq == String_Freq[mid]) {
-			String = mid + 1;
-		}
-
-		if (freq < String_Freq[mid]) {
-			if (mid > 0 && freq > String_Freq[mid-1]){
-				String = getClosest(String_Freq[mid-1], String_Freq[mid], freq, mid-1) + 1;
-			}
-			j = mid;
-		}
-
-		else {
-			if (mid < 5 && freq < String_Freq[mid+1]) {
-				String = getClosest(String_Freq[mid], String_Freq[mid+1], freq, mid) + 1;
-			}
-			m = mid + 1;
+//	int m = 0, j = 6, mid = 0;
+//	while (m < j) {
+//		mid = (m+j)/2;
+//		if (freq == String_Freq[mid]) {
+//			String = mid + 1;
+//		}
+//
+//		if (freq < String_Freq[mid]) {
+//			if (mid > 0 && freq > String_Freq[mid-1]){
+//				String = getClosest(String_Freq[mid-1], String_Freq[mid], freq, mid-1) + 1;
+//			}
+//			j = mid;
+//		}
+//
+//		else {
+//			if (mid < 5 && freq < String_Freq[mid+1]) {
+//				String = getClosest(String_Freq[mid], String_Freq[mid+1], freq, mid) + 1;
+//			}
+//			m = mid + 1;
+//		}
+//	}
+	float min_diff = 10000;
+	int string_ind = 0;
+	for(int i = 0; i < 6; i++) {
+		float diff = abs(real_freq - String_Freq[i]);
+		if(diff < min_diff) {
+			min_diff = diff;
+			string_ind = i;
 		}
 	}
-	char str_res[35];
-	float diff = freq - String_Freq[String - 1];
-	sprintf(str_res, "String %i, difference = %f", String, diff);
 
-	if (!String) String = String_Freq[mid];
+//	char str_res[35];
+//	float diff = freq - String_Freq[String - 1];
+//	sprintf(str_res, "String %i, difference = %.2f", String, diff);
+	float real_diff = real_freq - String_Freq[string_ind];
 
-	if (diff < -1) { //too high, make lower
-		char feedback[] = "\tToo high, make lower\n";
-		return concat(str_res, feedback);
-	}
-	if (diff > 1) { //too low, make higher
-		char feedback[] = "\tToo low, make it higher\n";
-		return concat(str_res, feedback);
-	}
-	char feedback[] = "\tTuned!\n";
-	return concat(str_res, feedback);
+//	if (!String) String = String_Freq[mid];
+
+//	if (diff < -10) { //too high, make lower
+//		char feedback[] = "\tToo high, make lower\n";
+//		return concat(str_res, feedback);
+//	}
+//	if (diff > 10) { //too low, make higher
+//		char feedback[] = "\tToo low, make it higher\n";
+//		return concat(str_res, feedback);
+//	}
+//	char feedback[] = "\tTuned!\n";
+//	return concat(str_res, feedback);
+
+	sprintf(str_res, "String = %i; real diff = %f, freq = %f\n", string_ind + 1, real_diff, real_freq);
+	PRINT(str_res);
+}
+
+
+char *decimal_to_binary(int n)
+{
+    int c, d, t;
+    char *p;
+
+    t = 0;
+    p = (char*)malloc(16+1);
+
+
+    for (c = 15 ; c >= 0 ; c--)
+    {
+        d = n >> c;
+
+        if (d & 1)
+            *(p+t) = 1 + '0';
+        else
+            *(p+t) = 0 + '0';
+
+        t++;
+    }
+    *(p+t) = '\0';
+
+    return  p;
 }
 /* USER CODE END 0 */
 
@@ -263,9 +299,9 @@ int main(void)
   /* USER CODE BEGIN 2 */
 
 //  HAL_I2S_Transmit_DMA(&hi2s3, &txBuf[0], 64);
-  HAL_I2S_Receive_DMA(&hi2s2, &pdmRxBuf[0],64); //pdm receive
+  HAL_I2S_Receive_DMA(&hi2s2, &pdmRxBuf[0], 64); //pdm receive
 
-  arm_rfft_fast_init_f32(&fft_handler, 2048);
+  arm_rfft_fast_init_f32(&fft_handler, FFT_SIZE);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -273,19 +309,35 @@ int main(void)
   while (1)
   {
 	  ReadSample();
-	  sprintf(str_res, "%f:%f\n", MidBuffer[0], MidBuffer[1]);
-	  PRINT(str_res);
-	  float freq = DoFFT_find_freq(&MidBuffer);
-//	  PRINT(detectString(freq));
+
+	  if(pcm_full) {
+//		  sprintf(str_res, "%i:%i:%i:%i:%i:%i:%i:%i:%i:%i:%i:%i:%i:%i:%i:%i\n",
+//				  pcm_buffer[0], pcm_buffer[1], pcm_buffer[2], pcm_buffer[3],
+//				  pcm_buffer[4], pcm_buffer[5], pcm_buffer[6], pcm_buffer[7],
+//				  pcm_buffer[8], pcm_buffer[17], pcm_buffer[10], pcm_buffer[11],
+//				  pcm_buffer[12], pcm_buffer[13], pcm_buffer[14], pcm_buffer[1023]);
+//		  PRINT(str_res);
+
+//		  sprintf(str_res, "%f:%f:%f:%f:%f:%f\n",
+//				  pcm_fl_buffer[0], pcm_fl_buffer[1], pcm_fl_buffer[2],pcm_fl_buffer[14], pcm_fl_buffer[1023]);
+//		  PRINT(str_res);
+
+		  float freq = DoFFT_find_freq();
+//		  PRINT(detectString(freq));
+		  detectString(freq);
+		  pcm_full = 0;
+	  }
+//	  HAL_Delay(200);
+
+//	  for(int i = 0; i < 16; i++) {
+//		  char *bin = decimal_to_binary(pcm_buffer[i]);
+//		  PRINT(bin);
+//		  PRINT("\n");
+//	  }
+//	  HAL_Delay(50);
+
 //	  PRINT(detectString(84.15));
 
-//    size_t ln = sizeof(txBuf)/sizeof(txBuf[0]);
-//    for (size_t i = 0; i < ln; i++){
-//    	char st[16];
-//    	itoa(txBuf[i], st, 16);
-//    	PRINT(st);
-//    }
-//    HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
